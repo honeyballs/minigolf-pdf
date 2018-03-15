c685bfaf48778479516e80f5911bd94359fb22c6
Mathias Widera <mathias.widera@akbs.de>
2018-03-15 11:40:46 +0100

    file upload und json parse eingebaut

--- src/App.js
+++ src/App.js
@@ -3,13 +3,39 @@ import Sidebar from "./components/Sidebar";
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    data: []
+  }
+
   render() {
     return (
       <div className="App">
-        <Sidebar />
+        <Sidebar files={this.state.files} loadFile={this.loadFile}/>
         <div id="pdf"></div>
       </div>
     );
+  }
+
+  loadFile = (files)=>{
+    if(!files.length){
+      console.log("no file uploaded")
+      return
+    }
+    let file = files[0]
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      //TODO: check file type for json, oder dropzone so einstellen das es nur json akzepziert
+      let reader = new FileReader()
+      reader.onload = ()=>{
+        //TODO: try catch for valid json
+        let data = JSON.parse(reader.result)
+        this.setState({data:data})
+      }
+      reader.readAsText(file)
+
+    } else {
+      console.log('The File APIs are not fully supported in this browser.')
+    }
   }
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Dropzone from 'react-dropzone'
 
 const Sidebar = props => {
+
+
     return (
         <div id="Sidebar">
-            <button>Load JSON</button>
+        <section>
+          <div className="dropzone">
+            <Dropzone onDrop={props.loadFile}>
+              <p>Try dropping some files here, or click to select files to upload.</p>
+            </Dropzone>
+          </div>
+          <aside>
+            <h2>Dropped file:</h2>
+            {props.file? props.file.name:''}
+          </aside>
+        </section>
         </div>
     );
 };
