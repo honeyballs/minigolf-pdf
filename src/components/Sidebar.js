242d0395ce2e9b6bc4c34383165f5acfa4e4fe5b
Yannick Bals <yannick.bals@web.de>
2018-03-15 10:46:06 +0100

    Add Sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = props => {
+    return (
+        <div id="Sidebar">
+            <button>Load JSON</button>
+        </div>
+    );
+};
+
+export default Sidebar;
