ae5da9a651d91b954b6bdde44c3c87312e6ff64b
Yannick Bals <yannick.bals@web.de>
2018-03-15 13:48:10 +0100

    Style changes, hide selects until data is loaded

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,26 +5,39 @@ import OptionList from "./OptionList";
 const Sidebar = props => {
   return (
     <div id="Sidebar">
-      <section>
-        <div className="dropzone" accept=".json">
-          <Dropzone onDrop={props.loadFile}>
-            <p>
-              Try dropping some files here, or click to select files to upload.
-            </p>
-          </Dropzone>
-        </div>
-      </section>
-      <OptionList
-        data={props.data}
-        selectedAnlagen={props.selectedAnlagen}
-        handleAnlagenChange={props.handleAnlagenChange}
-        selectedSpieler={props.selectedSpieler}
-        handleSpielerChange={props.handleSpielerChange}
-        selectedBahnen={props.selectedBahnen}
-        handleBahnenChange={props.handleBahnenChange}
-      />
+      <h1>Minigolf PDF</h1>
+      <p>Drag your JSON file here.</p>
+      <div className="dropzone" accept=".json">
+        <Dropzone onDrop={props.loadFile} style={dropStyle}>
+          <p>
+            Try dropping some files here, or click to select files to upload.
+          </p>
+        </Dropzone>
+      </div>
+      {props.data.length > 0 && (
+        <OptionList
+          data={props.data}
+          selectedAnlagen={props.selectedAnlagen}
+          handleAnlagenChange={props.handleAnlagenChange}
+          selectedSpieler={props.selectedSpieler}
+          handleSpielerChange={props.handleSpielerChange}
+          selectedBahnen={props.selectedBahnen}
+          handleBahnenChange={props.handleBahnenChange}
+        />
+      )}
     </div>
   );
+};
+
+const dropStyle = {
+  width: "100%",
+  height: "300px",
+  color: "#fff",
+  border: "2px dotted #fff",
+  borderRadius: "15px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center"
 };
 
 export default Sidebar;
