5b876edbba63e2f4b36aeddbf51a4d231eab38e6
Mathias Widera <mathias.widera@akbs.de>
2018-04-13 17:05:03 +0200

    resize der charts vor print, damit sie auf die Seite angepasst werden. Funktioniert anscheinend nicht im edge

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+function beforePrint () {
+  for (const id in window.Chart.instances) {
+    window.Chart.instances[id].resize()
+  }
+}
+
+if (window.matchMedia) {
+  let mediaQueryList = window.matchMedia('print')
+  mediaQueryList.addListener((mql) => {
+    if (mql.matches) {
+      beforePrint()
+    }
+  })
+}
+
+window.onbeforeprint = beforePrint
