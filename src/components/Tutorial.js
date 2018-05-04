bea43577c218b7874f2333209633e8bc3e4d931a
Mathias Widera <mathias.widera@akbs.de>
2018-05-04 10:48:08 +0200

    tutorial formatvorlage hinzugefügt

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -5,14 +5,16 @@ const Tutorial = props => {
         <div className="tutorial">
             <h2>Tutorial</h2>
             <p>Willkommen zum Minigolf PDF Generator. Bitte ziehen Sie eine JSON Datei in das Upload-Feld.</p>
-            <p>Anschließend können Sie über das Menü "Statistiken" das gewünschte Diagramm auswählen. 
+            <p>Die Datei muss das folgende Format einhalten:</p>
+            <img src="jsonformat.png"/>
+            <p>Anschließend können Sie über das Menü "Statistiken" das gewünschte Diagramm auswählen.
                 Nun befinden Sie sich in der Vorschau und können das Diagramm bearbeiten.</p>
             <p>Über die nun angezeigten Menüs können die Daten, die im Diagramm angezeigt werden, gefiltert werden.
                 Außerdem kann dem Diagramm noch ein eigener Titel gegeben werden. </p>
             <p>Wenn Sie mit dem Diagramm zufrieden sind, können Sie es mit einem Klick auf "Hinzufügen" in das Dokument einfügen.
                 Dieser Vorgang lässt sich beliebig oft wiederholen.</p>
-            <p>Ist das Dokument erstellt, so lässt sich das PDF über den "PDF Generieren" Button erzeugen. 
-                Wählen Sie im Dialog des Browsers "PDF" als Ausgabe.</p> 
+            <p>Ist das Dokument erstellt, so lässt sich das PDF über den "PDF Generieren" Button erzeugen.
+                Wählen Sie im Dialog des Browsers "PDF" als Ausgabe.</p>
         </div>
     )
 }
